2404d9360ad9a6672cf37e4618d4d93694a695dd
marven <2365702941@qq.com>
2020-11-26 09:56:14 +0800

    pp:spring security

---
+++ src/test/java/com/ppt/securitydemo1/Securitydemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppt.securitydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitydemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
