8e072baf0da60cc4740fe59b31860f13e61f7737
marven <2365702941@qq.com>
2020-11-26 16:58:11 +0800

    pp:spring security md

---
+++ src/main/java/com/ppt/securitydemo1/entity/Users.java
@@ -0,0 +1,40 @@
+package com.ppt.securitydemo1.entity;
+
+import lombok.Data;
+
+/**
+ * @author yzp
+ * @version 1.0
+ * @description: TODO
+ * @date 2020/11/26 15:17
+ */
+@Data
+public class Users {
+    private Integer id;
+    private String username;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
